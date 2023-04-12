1265189199b6e9a2b15a9d46b5a828baaa2c8a80
jackyyim881 <jackyyim881@hotmail.com>
2023-04-12 11:15:09 +0800

    upload

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ module "vnet" {
   location            = local.location
 }
 
+
+
 # Application Gateway
 module "app_gateway" {
   source = "./modules/app_gateway"
@@ -46,30 +48,37 @@ module "sql" {
 
 module "azure_front_door" {
   source = "./modules/azure_front_door"
-
+  resource_group_name = azurerm_resource_group.main.name
+  location            = local.location
   # 提供子模塊所需的變數值
 }
 
 module "azure_active_directory" {
   source = "./modules/azure_active_directory"
-
+  resource_group_name = azurerm_resource_group.main.name
+  location            = local.location
   # 提供子模塊所需的變數值
 }
 
 module "key_vault" {
   source = "./modules/key_vault"
-
+  resource_group_name = azurerm_resource_group.main.name
+  location            = local.location
   # 提供子模塊所需的變數值
 }
 
 module "ddos_protection" {
   source = "./modules/ddos_protection"
-
+  resource_group_name = azurerm_resource_group.main.name
+  location            = local.location
   # 提供子模塊所需的變數值
 }
 
 module "express_route" {
   source = "./modules/express_route"
-
+  resource_group_name = azurerm_resource_group.main.name
+  location            = local.location
   # 提供子模塊所需的變數值
 }
+
+
